9a523339551ad28db4bcb05ac0dc60935e5c7f24
Unknown <ryanjnoble@gmail.com>
2018-09-08 02:46:20 +0200

    Working on claim issuing

--- contracts/CCR-Core.sol
+++ contracts/CCR-Core.sol
@@ -1,20 +1,23 @@
 pragma solidity ^0.4.24;
 
+import "./EthereumClaimsRegistry.sol";
+
 contract CCRCore {
-    address public claimsRegistry;
+    EthereumClaimsRegistry public claimsRegistry;
     string public name;
     uint private quorum;
 
+    bytes32 public constant valid = keccak256("true");
+    bytes32 public constant invalid = keccak256("false");
 
     struct ClaimRace {
-        bytes claimTopic;
         address[] forClaim;
         address[] againstClaim;    
         bool pending;
         uint8 target;
     }
 
-    mapping(address => ClaimRace) private races;
+    mapping(address => mapping(string => ClaimRace)) private races;
     mapping(address => bool) private curators;
 
     event ClaimRaceRequested(address indexed _subject, string _claimTopic);
@@ -27,7 +30,7 @@ contract CCRCore {
 
     constructor(address _registry, string _name, address _owner) public {
         name = _name;
-        claimsRegistry = _registry;
+        claimsRegistry = EthereumClaimsRegistry(_registry);
         curators[_owner] = true;
         quorum = 1;
     }
@@ -36,20 +39,42 @@ contract CCRCore {
         return curators[_subject]; 
     }
 
-    function requestClaimRace(bytes _claim) public {
+    function requestClaimRace(string _claim) public {
         require(msg.sender != address(0), "Address invalid");
-        require(races[msg.sender].pending == false, "Claim race is underway");
-        races[msg.sender].pending = true;
-        races[msg.sender].claimTopic = _claim;
+        require(races[msg.sender][_claim].pending == false, "Claim race is underway");
+        races[msg.sender][_claim].pending = true;
     }
 
-    function cancelClaimRace() public{
-        require(races[msg.sender].pending == true, "Race not active");
-        delete races[msg.sender];
+    function cancelClaimRace(string _claim) public{
+        require(races[msg.sender][_claim].pending == true, "Race not active");
+        delete races[msg.sender][_claim];
     }
 
-    function voteOnClaim(address _subject, bytes _claim, bool _support)  public onlyCurator {
-        require(races[_subject].pending == true, "Claim race is not underway");
-        // require(races[_subject].claimTopic == _claim, "Claim mismatch");
+    function checkClaim(address _subject, string _claim) public view {
+        claimsRegistry.getClaim(address(this), _subject, keccak256(_claim));
     }
+
+    function voteOnClaim(address _subject, string _claim, bool _support)  public onlyCurator {
+        require(races[_subject][_claim].pending == true, "Claim race is not underway");
+        if(_support) {
+            races[_subject][_claim].forClaim.push(msg.sender);
+            if(races[_subject][_claim].forClaim.length == quorum) {
+                issueClaim(_subject, _claim, true);
+            }
+        } else {
+            races[_subject][_claim].againstClaim.push(msg.sender);
+            if(races[_subject][_claim].againstClaim.length == quorum) {
+                issueClaim(_subject, _claim, false);
+            }
+        }
+    }
+
+    function issueClaim(address _subject, string _claim, bool _passed) internal {
+        if(_passed){
+            claimsRegistry.setClaim(_subject, keccak256(_claim), valid);
+        } else {
+            claimsRegistry.setClaim(_subject, keccak256(_claim), invalid);
+        }
+    }
+     
 }
