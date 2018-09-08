8f0a5938a5803a3b9e91f61e32941bff4d55e600
Unknown <ryanjnoble@gmail.com>
2018-09-08 13:05:00 +0200

    Finished revoke functions, testing contracts

--- contracts/CCRCore.sol
+++ contracts/CCRCore.sol
@@ -19,6 +19,7 @@ contract CCRCore {
     struct ClaimRace {
         Vote votes;  
         bool pending;
+        bool revoking;
         uint8 target;
     }
 
@@ -32,7 +33,9 @@ contract CCRCore {
     mapping(address => Curator) private curators;
 
     event ClaimRaceRequested(address indexed _subject, string _claimTopic);
-    event RaceConcluded(address indexed _subject, string _claimTopic, bool _passed);
+    event ClaimRaceConcluded(address indexed _subject, string _claimTopic, bool _passed);
+
+    event RevokeClaimRequested(address indexed _subject,  string _claimTopic);
 
     event CuratorApplication(address indexed _applicant);
     event CuratorApplicationConcluded(address indexed _applicant, bool _passed);
@@ -42,21 +45,16 @@ contract CCRCore {
         _;
     }
 
-    constructor(address _registry, string _name, address _owner, uint _quorum) public {
+    constructor(address _registry, string _name, address _publisher, uint _quorum) public {
         name = _name;
         claimsRegistry = EthereumClaimsRegistry(_registry);
-        curators[_owner].validated = true;
+        curators[_publisher].validated = true;
         quorum = _quorum;
-        issueClaim(_owner, "Curator", true);
+        issueClaim(_publisher, "Curator", true);
     }
 
     function isCurator(address _subject) public view returns(bool) {
         return curators[_subject].validated; 
-    }
-
-     // Most likely too redundant 
-    function checkClaim(address _subject, bytes32 _claim) public view {
-        claimsRegistry.getClaim(address(this), _subject, _claim);
     }
 
     // Claim seekers
@@ -71,23 +69,55 @@ contract CCRCore {
         delete races[msg.sender][_claim];
     }
 
+    function revokeClaim(string _claim) public {
+        require(claimsRegistry.getClaim(address(this), msg.sender, keccak256(abi.encodePacked(_claim)))[0] != 0, "Claim not found in registry");
+        races[msg.sender][_claim].pending = true;
+        races[msg.sender][_claim].revoking = true;
+        emit RevokeClaimRequested(msg.sender, _claim);
+    }
+
    // Curator applications
+   // To manually initiate a curator
+    function addCurator(address _newCurator) public onlyCurator {
+        require(curators[msg.sender].validated == false, "Already curator");
+        require(curators[msg.sender].pending == false, "Vote already initiated"); 
+        curators[msg.sender].pending = true;
+        emit CuratorApplication(msg.sender);
+        voteOnApplicant(_newCurator, true);
+    }
+
+    // To initiate a vote to become a curator 
     function joinCurators() public {
         require(curators[msg.sender].validated == false, "Already curator");
-        require(curators[msg.sender].pending == false, "Vote initiated"); 
+        require(curators[msg.sender].pending == false, "Vote already initiated"); 
         curators[msg.sender].pending = true;
         emit CuratorApplication(msg.sender);
     }
 
-    function voteOnClaim(address _subject, string _claim, bool _support)  public onlyCurator {
+    // Can not cancel curator application as pending is not a blocker to any processes
+
+    function initiateRevokeClaim(address _subject, string _claim) public onlyCurator {
+        require(claimsRegistry.getClaim(address(this), _subject, keccak256(abi.encodePacked(_claim)))[0] != 0, "Claim not found in registry");
+        require(races[_subject][_claim].pending = false, "Claim race is underway");
+        races[_subject][_claim].pending = true;
+        races[_subject][_claim].revoking = true;
+        emit RevokeClaimRequested(_subject, _claim);
+    }
+    // Curators can vote validity of claims being issued
+    function voteOnClaim(address _subject, string _claim, bool _support) public onlyCurator {
         require(races[_subject][_claim].pending == true, "Claim race is not underway");
         if(_support) {
             if(notVoted(races[_subject][_claim].votes.inFavor, msg.sender) == false){
                 races[_subject][_claim].votes.inFavor.push(msg.sender);
                 if(checkQuorum(races[_subject][_claim].votes.inFavor.length, curatorCount, quorum)) {
-                    issueClaim(_subject, _claim, true);
-                    delete races[_subject][_claim];
-                    emit RaceConcluded(_subject, _claim, true);
+                    if(races[_subject][_claim].revoking) {
+                        revokeClaim(_subject, _claim);
+                        delete races[_subject][_claim];
+                    }else{
+                        issueClaim(_subject, _claim, true);
+                        delete races[_subject][_claim];
+                        emit ClaimRaceConcluded(_subject, _claim, true);
+                    }
                 }
             }
            
@@ -97,12 +127,13 @@ contract CCRCore {
                 if(checkQuorum(races[_subject][_claim].votes.against.length, curatorCount, quorum)) {
                     issueClaim(_subject, _claim, true);
                     delete races[_subject][_claim];
-                    emit RaceConcluded(_subject, _claim, false);
+                    emit ClaimRaceConcluded(_subject, _claim, false);
                 }
             }
         }
     }
 
+    // Voting on potential curators 
     function voteOnApplicant(address _subject, bool _support) public onlyCurator {
         require(curators[_subject].pending == true, "Application not initiated");
         // Leaving ability to vote good and bad, consequnce not present yet
@@ -135,7 +166,7 @@ contract CCRCore {
         quorumReached = (_voterCount/_quorum) + 1 <= _votes;
     }
 
-    // Refactor to hell
+    // Refactor probably
     function notVoted(address[] _list, address _target) internal pure returns(bool){
         for(uint i = 0; i < _list.length; i++){
             if(_target == _list[i]){
@@ -151,5 +182,9 @@ contract CCRCore {
         } else {
             claimsRegistry.setClaim(_subject, keccak256(abi.encodePacked(_claim)), invalid);
         }
+    }
+
+    function revokeClaim(address _subject, string _claim) internal {
+        claimsRegistry.removeClaim(address(this), _subject, keccak256(abi.encodePacked(_claim)));
     }
 }
